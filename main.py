f94603f52d60754928a61218e5ee809d4cee1d81
YashSakhareliya <sakhareliyayash@gmail.com>
2024-05-09 09:39:31 +0530

    bug fix with exception handling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ while True:
     User_action = input("Type add,show,edit,complete,exit: ")
     User_action = User_action.strip()
 
-    if 'add' in User_action:
+    if User_action.startswith("add"):
         todo = User_action[4:] + '\n'
 
         with open("todos.txt", "r") as File:
@@ -14,7 +14,7 @@ while True:
         with open("todos.txt", "w") as File:
             File.writelines(todos)
 
-    elif 'show' in User_action:
+    elif User_action.startswith("show"):
 
         with open("todos.txt", "r") as File:
             todos = File.readlines()
@@ -23,35 +23,43 @@ while True:
             todo = todo.strip('\n')
             print(f"{index+1} - {todo}")
 
-    elif 'edit' in User_action:
+    elif User_action.startswith("edit"):
 
-        number = int(User_action[5:])
-        number = number-1
+        try:
+            number = int(User_action[5:])
+            number = number-1
 
-        with open("todos.txt", "r") as File:
-            todos = File.readlines()
+            with open("todos.txt", "r") as File:
+                todos = File.readlines()
 
-        New_todo = input("Enter your new todo: ")
-        todos[number] = New_todo + "\n"
+            New_todo = input("Enter your new todo: ")
+            todos[number] = New_todo + "\n"
 
-        with open("todos.txt",'w') as File:
-            File.writelines(todos)
+            with open("todos.txt",'w') as File:
+                File.writelines(todos)
+        except ValueError:
+            print("Invalid command ")
+            continue
 
-    elif 'complete' in User_action:
-        number = int(User_action[9:])
-        with open("todos.txt", "r") as File:
-            todos = File.readlines()
-        index = number - 1
+    elif User_action.startswith("complate"):
+        try:
+            number = int(User_action[9:])
+            with open("todos.txt", "r") as File:
+                todos = File.readlines()
+            index = number - 1
 
-        Remove_todo =todos[index].strip('\n')
-        todos.pop(index)
+            Remove_todo =todos[index].strip('\n')
+            todos.pop(index)
 
-        with open("todos.txt", "w") as File:
-            File.writelines(todos)
+            with open("todos.txt", "w") as File:
+                File.writelines(todos)
 
-        print(f"Completed {Remove_todo}")
+            print(f"Completed {Remove_todo}")
+        except IndexError:
+            print("No item that index")
+            continue
 
-    elif 'exit' in User_action:
+    elif User_action.startswith("exit"):
         break
 
     else:
